cb9ab1840df6d1b151f9fadf711b950a118c8dd5
{Your_Name} <Clint Tuyor>
2025-03-12 19:34:04 +0800

    GET and POST Requests

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
+app.use(express.json());
+
+app.post('/submit', (req, res) => {
+    const data = req.body;
+    res.send(`Received data: ${JSON.stringify(data)}`);
+});
+
+app.get('/about', (req, res) => {
+    res.send('About page');
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
